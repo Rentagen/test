236a0f45d6c375ff51eb37bc1559c22658dffaf5
Ivan <somethingtim98@gmail.com>
2020-05-09 07:48:01 +0800

    Это первый коммит

---
+++ Food/styles/script.js
@@ -0,0 +1,42 @@
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+
+console.log(4/0);
+console.log('string' * 9);
+
+let persone = {
+  name: "Jhon",
+  age: 25,
+  isMarried:false
+};
+
+console.log(persone.name);
+
+let arr = ['apple', 'orange', 'green'];
+
+console.log(arr[2]);
+
+//alert("Hello!");
+
+//confirm("Are u here?");
+
+// let answer = prompt("Есть ли вам 18 лет?", "");
+//
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" === 2)
